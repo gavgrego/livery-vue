acf509a310410be017aaa71e479d4287813e022b
Gavin Gregory <gavgrego@gmail.com>
2025-08-31 10:29:48 -0700

    update playground with all button variants

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -4,6 +4,15 @@ import { Button } from "@gavgrego/livery-vue";
 
 <template>
   <div class="main flex flex-col max-w-75 gap-2.5 m-2.5">
-    <Button title="Button">Hello</Button>
+    <h3 class="text-xl font-bold">Button</h3>
+    <Button title="Button">Default</Button>
+    <Button variant="destructive">Destructive</Button>
+    <Button variant="outline">Outline</Button>
+    <Button variant="secondary">Secondary</Button>
+    <Button variant="ghost">Ghost</Button>
+    <Button variant="link">Link</Button>
+    <Button variant="default" size="sm">Small</Button>
+    <Button variant="default" size="lg">Large</Button>
+    <Button variant="default" size="icon">Icon</Button>
   </div>
 </template>
