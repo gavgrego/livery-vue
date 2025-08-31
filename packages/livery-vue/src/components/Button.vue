3106430727aa4cd01963241e5044a0ea30e9907e
Gavin Gregory <gavgrego@gmail.com>
2025-08-31 09:51:44 -0700

    attempt to fix import alias for utils file

---
+++ packages/livery-vue/src/components/Button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+type Props = {
+  title: string;
+};
+
+const props = defineProps<Props>();
+
+console.log(props);
+</script>
+
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
