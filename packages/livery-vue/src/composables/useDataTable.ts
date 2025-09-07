a4de2702887e752cb1b80c4da96f20335a787807
Gavin Gregory <gavgrego@gmail.com>
2025-09-06 17:35:51 -0700

    clean up, remove dummy data, let vuetable manage pagination, dummy sorting

--- packages/livery-vue/src/composables/useDataTable.ts
+++ packages/livery-vue/src/composables/useDataTable.ts
@@ -5,30 +5,18 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useVueTable,
-  createColumnHelper,
   type ColumnDef,
+  type SortingState,
 } from "@tanstack/vue-table";
-
-type Award = {
-  date: string;
-  lastSeen: string;
-  origin: string;
-  destination: string;
-  economy: number;
-  premium: number;
-  business: number;
-  first: number;
-};
 
 type UseDataTableProps<T> = {
   data: Ref<T[]>;
   columns: ColumnDef<T>[];
 };
 
-const useDataTable = <T>({
-  data,
-  columns,
-}: UseDataTableProps<T>) => {
+const useDataTable = <T>({ data, columns }: UseDataTableProps<T>) => {
+  const sorting = ref<SortingState>([]);
+
   const dataTable = useVueTable({
     data,
     columns,
@@ -36,6 +24,9 @@ const useDataTable = <T>({
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting: sorting.value,
+    },
   });
 
   return {
@@ -43,65 +34,4 @@ const useDataTable = <T>({
   };
 };
 
-// Example usage with Award data
-const dummyAwardData = ref<Award[]>([
-  {
-    date: "2025-01-01",
-    lastSeen: "2025-01-01",
-    origin: "LAX",
-    destination: "SFO",
-    economy: 5000,
-    premium: 10000,
-    business: 20000,
-    first: 35000,
-  },
-  {
-    date: "2025-01-02",
-    lastSeen: "2025-01-02",
-    origin: "SFO",
-    destination: "LAX",
-    economy: 10000,
-    premium: 30000,
-    business: 4000,
-    first: 56000,
-  },
-]);
-
-const awardColumnHelper = createColumnHelper<Award>();
-
-const awardColumns = [
-  awardColumnHelper.accessor("date", {
-    id: "date",
-    header: "Date",
-  }),
-  awardColumnHelper.accessor("lastSeen", {
-    id: "lastSeen",
-    header: "Last Seen",
-  }),
-  awardColumnHelper.accessor("origin", {
-    id: "origin",
-    header: "Origin",
-  }),
-  awardColumnHelper.accessor("destination", {
-    id: "destination",
-    header: "Destination",
-  }),
-  awardColumnHelper.accessor("economy", {
-    id: "economy",
-    header: "Economy",
-  }),
-  awardColumnHelper.accessor("premium", {
-    id: "premium",
-    header: "Premium",
-  }),
-  awardColumnHelper.accessor("business", {
-    id: "business",
-    header: "Business",
-  }),
-  awardColumnHelper.accessor("first", {
-    id: "first",
-    header: "First",
-  }),
-];
-
-export { useDataTable as default, dummyAwardData, awardColumns };
+export default useDataTable;
